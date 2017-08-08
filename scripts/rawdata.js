d6af1e48a9d3e6db94d0ae8310aa992145878564
Joel Epstein <joel.epstein2@gmail.com>
2017-08-07 17:30:57 -0700

    previously added icomoon icons and a css normalize... added an object constructor and a sample setup for him

---
+++ scripts/rawdata.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var rawData = [
+  {
+    title:       'About Me',
+    category:    'technology',
+    author:      'Larry Cessna',
+    projectUrl:   'https://github.com/snlcessna/About-Me',
+    publishedOn: '2017-06-07',
+    body:        '<p>This page is a little about me</p>',
+  }];
